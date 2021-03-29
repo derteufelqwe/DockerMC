dbdcaf22164567c7c1b145e2e1382d5b1a79c582
Arne M <derteufelqwe@gmail.com>
2021-03-29 15:46:30 +0200

    - Add github actions

--- DockerLogDriver/buildImage.sh
+++ DockerLogDriver/buildImage.sh
@@ -11,6 +11,7 @@ docker export "$id" | tar -x -C rootfs
 docker rm -vf "$id"
 
 echo "Building plugin..."
-docker plugin create derteufelqwe/dockermc-log-driver:latest .
+docker plugin create "derteufelqwe/dockermc-log-driver:$VERSION" .
+docker plugin create "derteufelqwe/dockermc-log-driver:latest" .
 
 echo "Created dockermc-log plugin"
